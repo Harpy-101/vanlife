6c1be1d82129af2b2be67305a482f59c4461f402
David Israel <davidinp6@gmail.com>
2025-03-17 23:30:51 +0200

    feat: add a link  for the "find your van" button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <>
             <div className="hero">
                 <h1>You got the travel plans, we got the travel vans.</h1>
                 <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-                <button className="search-van-btn">Find your van</button>
+                <Link to="/vans"><button className="search-van-btn">Find your van</button></Link>
             </div>
         </>
     )
